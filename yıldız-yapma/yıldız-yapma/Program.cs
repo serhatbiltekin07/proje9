602a42bd34a88e55f6d0cc8208143b621b70446c
BİLTEKİN KURTULUŞ <112376639+serhatbiltekin07@users.noreply.github.com>
2023-08-25 21:45:17 +0300

    Add files via upload

---
+++ yıldız-yapma/yıldız-yapma/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yıldız_yapma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int satir, sutun;
+            Console.Write("Satır sayısı:");
+            satir = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Sütun sayısı:");
+            sutun = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            for( int  i= 0; i <sutun; i ++)
+            {
+                for(int j =0; j < satir; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            
+            Console.ReadKey();
+            
+
+        }
+    }
+}
